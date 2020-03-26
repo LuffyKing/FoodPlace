0c19617004f0dedefc9eec3cd8674fa4e6dc94c1
ASHWHX <wanghaixin1993@126.com>
2020-03-26 13:30:23 +0000

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,29 @@
+package FoodPlace;
+
+    public class Menu {
+
+    public StoreMuneItems() {
+
+    }
+
+    public CreateNewMenuItem() {
+
+    }
+
+    public RemoveMenu() {
+
+    }
+
+    public EditMenuItem() {
+
+    }
+
+    public CreateSpecials() {
+
+    }
+    
+    public RemoveSpecials() {
+
+    }
+    }
+    
