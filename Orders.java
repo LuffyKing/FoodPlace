5ed85c62d820468b5a260812d2a168c7eb4b36c6
wadhaalkubaisi <61501573+wadhaalkubaisi@users.noreply.github.com>
2020-03-30 15:18:32 +0100

    Add files via upload

---
+++ Orders.java
@@ -0,0 +1,44 @@
+package FoodPlace;
+
+public class Orders {
+   public void orderId (){
+       public void CreateorderId(){
+           return orderId;
+       }
+       public void Create Customers_id(){
+           return Customers_id;
+       }
+       public void Add List_Menu(){
+
+       }
+       public Edit List_Menu(){
+
+       }
+       public void  Create order_status() {
+           public Create new_order_status
+           return order_status;
+       }
+       public void  Create Menu_id(){
+
+       }
+       public void  Set Ordertime_date{
+
+       }
+
+       public void  Create Order_Total(){
+       }
+
+       public void Set Print_Receipt(){
+
+       }
+
+       }
+
+
+
+   }
+
+
+
+
+
