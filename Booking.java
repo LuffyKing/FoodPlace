6c17d81156a3ef2a8ae38a8de1fcfa8ea2eaa0cd
sara-r-p <62297420+sara-r-p@users.noreply.github.com>
2020-04-08 21:55:43 +0100

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -1,19 +1,25 @@
 package FoodPlace;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
 public class Booking {
     public static final int defaultBookingLength = 1;
 
     private final int bookingID;
     private int numberOfGuests;
-    //private ??? timeOfBooking;
+    private LocalDateTime bookingTime;
     private int bookingLength;
     private boolean approved;
     private final int customerID;
 
-    public Booking(int numberOfGuests_, int bookingLength_, final int customerID_){
+    public Booking(int numberOfGuests_, int bookingLength_, LocalDateTime bookingTime_, final int customerID_){
         numberOfGuests = numberOfGuests_;
         bookingLength = bookingLength_;
         approved = false;
+        bookingTime = bookingTime_;
         customerID = customerID_;
         //get from DB after save
         bookingID = -1;
@@ -26,8 +32,6 @@ public class Booking {
     public int getNumberOfGuests() {
         return numberOfGuests;
     }
-
-
 
     public int setNumberOfGuests(final int newGuestNumber) {
         if (newGuestNumber > 0) {
@@ -47,6 +51,17 @@ public class Booking {
         return bookingLength;
     }
 
+     public LocalDateTime getBookingTime() {
+        return bookingTime;
+    }
+
+    public LocalDateTime setBookingTime(LocalDateTime newBookingTime) {
+        if (newBookingTime > java.time.LocalDateTime.now()) {
+            bookingTime = newBookingTime;
+        }
+        return bookingTime;
+    }
+    
     public boolean getApproved() {
         return approved;
     }
