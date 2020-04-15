8874b3dcb7f05529cc1cd95865d94ebaf8034b30
sara-r-p <62297420+sara-r-p@users.noreply.github.com>
2020-04-15 21:48:35 +0100

    Update ControllerBookingHome.java

--- Controller/ControllerBookingHome.java
+++ Controller/ControllerBookingHome.java
@@ -6,11 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
+/**
+*Controller for the scene the customer views when they click on the My Bookings button.
+*@author Sara Philipson
+*@version ?
+*/
 public class ControllerBookingHome {
-
 
     public Button linktomyaccount;
     public Button linktomenu;
@@ -20,6 +23,10 @@ public class ControllerBookingHome {
     public Button logoutbutton;
     public Button linktobookingcreate;
 
+    /**
+    *Changes the scene the customer can see to the scene where they can create a booking.
+    *@param event Clicking on the Create Booking button.
+    */
     public void linktobookingcreateButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent bookingCreateParent = FXMLLoader.load(getClass().getResource("bookingCreate.fxml"));
         Scene bookingCreateScene = new Scene(bookingCreateParent);
@@ -28,6 +35,10 @@ public class ControllerBookingHome {
         window.show();
     }
 
+    /**
+    *Changes the scene the customer can see to the My Account scene.
+    *@param event Clicking on the My Account button.
+    */
     public void linktomyaccountButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent myaccountParent = FXMLLoader.load(getClass().getResource("MyAccount.fxml"));
         Scene myaccountScene = new Scene(myaccountParent);
@@ -36,6 +47,10 @@ public class ControllerBookingHome {
         window.show();
     }
 
+    /**
+    *Changes the scene the customer can see to the Menu scene.
+    *@param event Clicking on the Menu button.
+    */
     public void linktomenuButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent menuParent = FXMLLoader.load(getClass().getResource("customerMenu.fxml"));
         Scene menuScene = new Scene(menuParent);
@@ -44,6 +59,10 @@ public class ControllerBookingHome {
         window.show();
     }
 
+    /**
+    *Changes the scene the customer can see to the My Orders scene.
+    *@param event Clicking on the My Orders button.
+    */
     public void linktomyordersButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent myordersParent = FXMLLoader.load(getClass().getResource("myOrdersHome.fxml"));
         Scene myordersScene = new Scene(myordersParent);
@@ -52,6 +71,10 @@ public class ControllerBookingHome {
         window.show();
     }
 
+    /**
+    *Changes the scene the customer can see to the My Bookings scene.
+    *@param event Clicking on the My Bookings button.
+    */
     public void linktobookinghomeButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent mybookingsParent = FXMLLoader.load(getClass().getResource("bookingHome.fxml"));
         Scene mybookingsScene = new Scene(mybookingsParent);
@@ -60,6 +83,10 @@ public class ControllerBookingHome {
         window.show();
     }
 
+    /**
+    *Changes the scene the customer can see to the Notifications scene.
+    *@param event Clicking on the Notifications button.
+    */
     public void notificationspageButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent notificationsParent = FXMLLoader.load(getClass().getResource("Notification.fxml"));
         Scene notificationsScene = new Scene(notificationsParent);
@@ -68,6 +95,10 @@ public class ControllerBookingHome {
         window.show();
     }
 
+    /**
+    *Changes the scene the customer can see to the Customer Login scene.
+    *@param event Clicking on the Logout button.
+    */
     public void logoutbuttonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent logoutParent = FXMLLoader.load(getClass().getResource("Customer_login_Scene.fxml"));
         Scene logoutScene = new Scene(logoutParent);
