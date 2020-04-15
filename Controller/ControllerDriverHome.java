674527a8c1056a511abd19844cfe8ff3d57e64fc
sara-r-p <62297420+sara-r-p@users.noreply.github.com>
2020-04-15 22:04:39 +0100

    Update ControllerDriverHome.java

--- Controller/ControllerDriverHome.java
+++ Controller/ControllerDriverHome.java
@@ -6,9 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
+/**
+*Controller for the scene a Delivery Driver gets when they log in.
+*@author Sara Philipson
+*@version ?
+*/
 public class ControllerDriverHome {
 
     public Button linktostaff;
@@ -19,6 +23,10 @@ public class ControllerDriverHome {
     public Button notificationspage;
     public Button logoutbutton;
 
+    /**
+    *Changes the scene to the Driver Notifications scene.
+    *@param event Clicking on the Notifications button.
+    */
     public void notificationspageButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent notificationsParent = FXMLLoader.load(getClass().getResource("------.fxml"));
         Scene notificationsScene = new Scene(notificationsParent);
@@ -27,6 +35,10 @@ public class ControllerDriverHome {
         window.show();
     }
 
+    /**
+    *Changes the scene to the Staff Login scene.
+    *@param event Clicking on the Logout button.
+    */
     public void logoutbuttonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent logoutParent = FXMLLoader.load(getClass().getResource("Staff_login_Draft.fxml"));
         Scene logoutScene = new Scene(logoutParent);
@@ -34,6 +46,11 @@ public class ControllerDriverHome {
         window.setScene(logoutScene);
         window.show();
     }
+    
+    /**
+    *Changes the scene to the Driver permission denied scene.
+    *@param event Clicking on a button that leads to a scene that the driver doesn't have access to.
+    */
     public void permissiondeniedscreen(javafx.event.ActionEvent event) throws IOException {
         Parent permissiondeniedParent = FXMLLoader.load(getClass().getResource("permissiondenieddriver.fxml"));
         Scene permissiondeniedScene = new Scene(permissiondeniedParent);
@@ -42,6 +59,10 @@ public class ControllerDriverHome {
         window.show();
     }
 
+    /**
+    *Changes the scene to the Driver Orders scene.
+    *@param event Clicking on the Orders button.
+    */
     public void linktoordersdriverview(javafx.event.ActionEvent event) throws IOException {
         Parent ordersdriverviewParent = FXMLLoader.load(getClass().getResource("ordersDriver.fxml"));
         Scene ordersdriverviewScene = new Scene(ordersdriverviewParent);
