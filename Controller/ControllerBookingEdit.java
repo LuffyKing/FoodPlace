2f55b3018f9a4075854085845a03c81dd82fdfab
sara-r-p <62297420+sara-r-p@users.noreply.github.com>
2020-04-15 22:46:42 +0100

    Add files via upload

---
+++ Controller/ControllerBookingEdit.java
@@ -0,0 +1,118 @@
+package FoodPlace;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+/**
+ *Controller for the scene the customer views when they go to edit a booking.
+ *@author Sara Philipson
+ *@version ?
+ */
+public class ControllerBookingEdit {
+
+    public Button linktomyaccount;
+    public Button linktomenu;
+    public Button linktomyorders;
+    public Button linktobookinghome;
+    public Button notificationspage;
+    public Button logoutbutton;
+    public DatePicker bookigdatepicker;
+    public ComboBox bookingtimepicker;
+    public ComboBox bookingguestspicker;
+    public ComboBox bookinglengthpicker;
+    public Button linktobookingsubmitted;
+
+    /**
+     *Changes the scene the customer can see to the My Account scene.
+     *@param event Clicking on the My Account button.
+     */
+    public void linktomyaccountButtonPressed(javafx.event.ActionEvent event) throws IOException {
+        Parent myaccountParent = FXMLLoader.load(getClass().getResource("MyAccount.fxml"));
+        Scene myaccountScene = new Scene(myaccountParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(myaccountScene);
+        window.show();
+    }
+
+    /**
+     *Changes the scene the customer can see to the Menu scene.
+     *@param event Clicking on the Menu button.
+     */
+    public void linktomenuButtonPressed(javafx.event.ActionEvent event) throws IOException {
+        Parent menuParent = FXMLLoader.load(getClass().getResource("customerMenu.fxml"));
+        Scene menuScene = new Scene(menuParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(menuScene);
+        window.show();
+    }
+
+    /**
+     *Changes the scene the customer can see to the My Orders scene.
+     *@param event Clicking on the My Orders button.
+     */
+    public void linktomyordersButtonPressed(javafx.event.ActionEvent event) throws IOException {
+        Parent myordersParent = FXMLLoader.load(getClass().getResource("myOrdersHome.fxml"));
+        Scene myordersScene = new Scene(myordersParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(myordersScene);
+        window.show();
+    }
+
+    /**
+     *Changes the scene the customer can see to the My Bookings scene.
+     *@param event Clicking on the My Bookings button.
+     */
+    public void linktobookinghomeButtonPressed(javafx.event.ActionEvent event) throws IOException {
+        Parent mybookingsParent = FXMLLoader.load(getClass().getResource("bookingHome.fxml"));
+        Scene mybookingsScene = new Scene(mybookingsParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(mybookingsScene);
+        window.show();
+    }
+
+    /**
+     *Changes the scene the customer can see to the Notification scene.
+     *@param event Clicking on the Notification button.
+     */
+    public void notificationspageButtonPressed(javafx.event.ActionEvent event) throws IOException {
+        Parent notificationsParent = FXMLLoader.load(getClass().getResource("Notification.fxml"));
+        Scene notificationsScene = new Scene(notificationsParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(notificationsScene);
+        window.show();
+    }
+
+    /**
+     *Changes the scene the customer can see to the Customer Login scene.
+     *@param event Clicking on the Logout button.
+     */
+    public void logoutbuttonPressed(javafx.event.ActionEvent event) throws IOException {
+        Parent logoutParent = FXMLLoader.load(getClass().getResource("Customer_login_Scene.fxml"));
+        Scene logoutScene = new Scene(logoutParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(logoutScene);
+        window.show();
+    }
+
+    /**
+     *Changes the scene the customer can see to the Booking Submitted scene.
+     *@param event Clicking on the Confirm button to confirm the creation of a booking.
+     */
+    public void linktobookingsubmittedButtonPressed(javafx.event.ActionEvent event) throws IOException {
+        Parent bookingsubmittedParent = FXMLLoader.load(getClass().getResource("bookingSubmitted.fxml"));
+        Scene bookingsubmittedScene = new Scene(bookingsubmittedParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(bookingsubmittedScene);
+        window.show();
+    }
+
+    //method for actual edit of booking still in progress
+
+}
