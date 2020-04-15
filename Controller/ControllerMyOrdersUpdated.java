290e36655ae30c3763ba8a485a9391399348608b
sara-r-p <62297420+sara-r-p@users.noreply.github.com>
2020-04-15 22:26:45 +0100

    Update ControllerMyOrdersUpdated.java

--- Controller/ControllerMyOrdersUpdated.java
+++ Controller/ControllerMyOrdersUpdated.java
@@ -6,10 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
+/**
+*Controller for the scene the customer sees after completing the edits on their order.
+*@author Sara Philipson
+*@version ?
+*/
 public class ControllerMyOrdersUpdated {
+  
     public Button linktomyaccount;
     public Button linktomenu;
     public Button linktomyorders;
@@ -18,6 +23,10 @@ public class ControllerMyOrdersUpdated {
     public Button logoutbutton;
     public Button linkbacktomyordershome;
 
+    /**
+    *Changes the customer's scene to the My Account scene.
+    *@param event Clicking on the My Account button.
+    */
     public void linktomyaccountButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent myaccountParent = FXMLLoader.load(getClass().getResource("MyAccount.fxml"));
         Scene myaccountScene = new Scene(myaccountParent);
@@ -26,6 +35,10 @@ public class ControllerMyOrdersUpdated {
         window.show();
     }
 
+    /**
+    *Changes the customer's scene to the Menu scene.
+    *@param event Clicking on the Menu button.
+    */
     public void linktomenuButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent menuParent = FXMLLoader.load(getClass().getResource("customerMenu.fxml"));
         Scene menuScene = new Scene(menuParent);
@@ -34,6 +47,10 @@ public class ControllerMyOrdersUpdated {
         window.show();
     }
 
+    /**
+    *Changes the customer's scene to the My Orders scene.
+    *@param event Clicking on the My Orders button.
+    */
     public void linktomyordersButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent myordersParent = FXMLLoader.load(getClass().getResource("myOrdersHome.fxml"));
         Scene myordersScene = new Scene(myordersParent);
@@ -42,6 +59,10 @@ public class ControllerMyOrdersUpdated {
         window.show();
     }
 
+    /**
+    *Changes the customer's scene to the My Bookings scene.
+    *@param event Clicking on the My Bookings button.
+    */
     public void linktobookinghomeButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent mybookingsParent = FXMLLoader.load(getClass().getResource("bookingHome.fxml"));
         Scene mybookingsScene = new Scene(mybookingsParent);
@@ -50,6 +71,10 @@ public class ControllerMyOrdersUpdated {
         window.show();
     }
 
+    /**
+    *Changes the customer's scene to the Notifications scene.
+    *@param event Clicking on the Notifications button.
+    */
     public void notificationspageButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent notificationsParent = FXMLLoader.load(getClass().getResource("Notification.fxml"));
         Scene notificationsScene = new Scene(notificationsParent);
@@ -58,6 +83,10 @@ public class ControllerMyOrdersUpdated {
         window.show();
     }
 
+    /**
+    *Changes the customer's scene to the customer login scene.
+    *@param event Clicking on the Logout button.
+    */
     public void logoutbuttonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent logoutParent = FXMLLoader.load(getClass().getResource("Customer_login_Scene.fxml"));
         Scene logoutScene = new Scene(logoutParent);
