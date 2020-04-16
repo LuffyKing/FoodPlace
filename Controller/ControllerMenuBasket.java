19642fb860f6c8a63dd97c6412e0c8bf7c6062f7
ASHWHX <wanghaixin1993@126.com>
2020-04-16 23:28:47 +0100

    Add files via upload

--- Controller/ControllerMenuBasket.java
+++ Controller/ControllerMenuBasket.java
@@ -2,7 +2,13 @@ package FoodPlace.Controller;
 
 import java.io.IOException;
 
-public class ControllerMenu_Scene {
+/**
+ *Controller for the scene a Delivery Driver gets when they log in.
+ *@author Haixin Wang
+ *@version ?
+ */
+
+public class ControllerMenuBasket {
     public Button linktostaff;
     public Button linktoorders;
     public Button linktomenu;
@@ -12,6 +18,10 @@ public class ControllerMenu_Scene {
     public Button logoutbutton;
     public Button linktomyordershome;
 
+    /**
+     *Changes the scene to the permissiondenied scene.
+     *@param event Clicking on the staff button.
+     */
     public void linktostaffButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent staffParent = FXMLLoader.load(getClass().getResource("permissiondeniedwaiter.fxml"));
         Scene staffScene = new Scene(staffParent);
@@ -20,6 +30,10 @@ public class ControllerMenu_Scene {
         window.show();
     }
 
+    /**
+     *Changes the scene to the ordersChef scene.
+     *@param event Clicking on the orders button.
+     */
     public void linktoordersdriverButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent ordersParent = FXMLLoader.load(getClass().getResource("WaiterOrderHomepage.fxml"));
         Scene ordersScene = new Scene(ordersParent);
@@ -28,6 +42,10 @@ public class ControllerMenu_Scene {
         window.show();
     }
 
+    /**
+     *Changes the scene to the Menu_Scene scene.
+     *@param event Clicking on the menu button.
+     */
     public void linktomenusceneButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent menuParent = FXMLLoader.load(getClass().getResource("menuWaiter.fxml"));
         Scene menuScene = new Scene(menuParent);
@@ -36,6 +54,10 @@ public class ControllerMenu_Scene {
         window.show();
     }
 
+    /**
+     *Changes the scene to the permissiondeniedchef scene.
+     *@param event Clicking on the bookings button.
+     */
     public void linktobookinghomeButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent bookingsParent = FXMLLoader.load(getClass().getResource("staffBookingHome.fxml"));
         Scene bookingsScene = new Scene(bookingsParent);
@@ -44,6 +66,10 @@ public class ControllerMenu_Scene {
         window.show();
     }
 
+    /**
+     *Changes the scene to the permissiondeniedchef scene.
+     *@param event Clicking on the reports button.
+     */
     public void linktoreportsButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent reportsParent = FXMLLoader.load(getClass().getResource("permissiondeniedwaiter.fxml"));
         Scene reportsScene = new Scene(reportsParent);
@@ -52,6 +78,10 @@ public class ControllerMenu_Scene {
         window.show();
     }
 
+    /**
+     *Changes the scene to the Chef Notifications scene.
+     *@param event Clicking on the Notifications button.
+     */
     public void notificationspageButtonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent notificationsParent = FXMLLoader.load(getClass().getResource("WaiterNotifications.fxml"));
         Scene notificationsScene = new Scene(notificationsParent);
@@ -60,6 +90,10 @@ public class ControllerMenu_Scene {
         window.show();
     }
 
+    /**
+     *Changes the scene to the Staff login Draft scene.
+     *@param event Clicking on the Logout button.
+     */
     public void logoutbuttonPressed(javafx.event.ActionEvent event) throws IOException {
         Parent logoutParent = FXMLLoader.load(getClass().getResource("Staff_login_Draft.fxml"));
         Scene logoutScene = new Scene(logoutParent);
@@ -68,8 +102,12 @@ public class ControllerMenu_Scene {
         window.show();
     }
 
+    /**
+     *Changes the scene to the myOrdersHome scene.
+     *@param event Clicking on the Create Order button.
+     */
     public void linktomyordershomeButtonPressed(javafx.event.ActionEvent event) throws IOException {
-        Parent createParent = FXMLLoader.load(getClass().getResource("WaiterOrderHomepage.fxml"));
+        Parent createParent = FXMLLoader.load(getClass().getResource("myOrdersHome.fxml"));
         Scene createScene = new Scene(createParent);
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(createScene);
