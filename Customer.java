8f69503ac1dfd82aaf39e9c24d895246632106ec
sara-r-p <62297420+sara-r-p@users.noreply.github.com>
2020-04-16 23:27:51 +0100

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -3,10 +3,22 @@ package FoodPlace;
 import java.time.LocalDateTime;
 import javafx.collections.ObservableList;
 
+/**
+*This class represents a customer who is a person and all the functions specific to them.
+*@author Damola Aderinwale (JavaDoc by Sara Philipson)
+*@version ?
+*/
 public class Customer extends Person{
     private String address;
     private int customerId;
 
+    /**
+    *Constructor for the customer, some details obtained from person class.
+    *@param fname The first name of the customer.
+    *@param lname The last name of the customer.
+    *@param add The customer address.
+    *@param cid The customer Id.
+    */
     public Customer(String fname, String lname, String add, int cid) {
         super(fname, lname);
         address = add;
@@ -14,30 +26,65 @@ public class Customer extends Person{
         customerId = cid;
     }
 
+    /**
+    *Returns the customer Id
+    *@return The customer Id.
+    */
     public int getCustomerId() {
         return customerId;
     }
 
+    /**
+    *Updates the customer Id.
+    *@param customerId The new customer Id
+    */
     public void setCustomerId(int customerId) {
         this.customerId = customerId;
     }
 
+    /**
+    *Returns the customer's address.
+    *@return The customer's address.
+    */
     public String getAddress() {
         return address;
     }
 
+    /**
+    *Updates a customer's address.
+    *@param add The customer's address.
+    */
     public void setAddress(String add) {
         address = add;
     }
 
+    /**
+    *Creates a new delivery order.
+    *@param items A list of items in the order.
+    *@param orderId The Id of the order.
+    *@param dateTime The date and time of the order.
+    *@param orders The list of orders to add the order to.
+    */
     public void addOrderDelivery(ObservableList<OrderItem> items, int orderId, LocalDateTime dateTime, ObservableList<Order> orders){
         Delivery delivery = new Delivery(items, orderId, dateTime,address);
         orders.add(delivery);
-
     }
+    
+    /**
+    *Removes an order from the order list.
+    *@param order The order to be removed.
+    *@param orders The list from whichthe order will be removed.
+    */
     public void removeOrder(Order order, ObservableList<Order> orders){
         orders.removeIf(anOrder -> order.getOrderId() == anOrder.getOrderId());
     }
+    
+    /**
+    *Function to edit a delivery order.
+    *@param delivery The delivery order to be edited.
+    *@param items The list of items in the order.
+    *@param address The delivery address.
+    */
     public void editOrderDelivery(Delivery delivery, ObservableList<OrderItem> items, String address){
         if(items != null){
             if (!delivery.getCompletionStatus()){
@@ -54,6 +101,15 @@ public class Customer extends Person{
             }
         }
     }
+    
+    /**
+    *Function to create a takeaway order.
+    *@param items The list of items in the order.
+    *@param orderId The order ID.
+    *@param dateTime The date and time of the order.
+    *@param ptime The pickup time.
+    *@param orders The order list the order will be added to.
+    */
     public void addOrderTakeAway(ObservableList<OrderItem> items, int orderId, LocalDateTime dateTime ,LocalDateTime ptime, ObservableList<Order> orders){
         Takeaway takeaway = new Takeaway(items, orderId, dateTime, ptime);
         orders.add(takeaway);
@@ -75,11 +131,24 @@ public class Customer extends Person{
         }
     }
 
-
+    /**
+    *The function to create a booking.
+    *@param numberOfGuests_ The number of guests in the booking.
+    *@param bookingTime_ The date and time the booking is for.
+    *@param bookingList The list the booking will be added to.
+    *@param bookingLength_ The length of the booking in hours.
+    *@param bookingId The booking ID.
+    */
     public void createBooking(int numberOfGuests_, int bookingLength_, LocalDateTime bookingTime_, ObservableList<Booking> bookingList, int bookingId){
         Booking booking = new Booking(numberOfGuests_, bookingLength_, bookingTime_, customerId, bookingId);
         bookingList.add(booking);
     }
+    
+    /**
+    *Function to cancel a booking.
+    *@param booking The booking to be cancelled.
+    *@param bookinglist The list from which the booking will be deleted.
+    */
     public void cancelBooking(Booking booking, ObservableList<Booking> bookingList){
         bookingList.removeIf(aBooking -> aBooking.getBookingID() == booking.getBookingID());
     }
