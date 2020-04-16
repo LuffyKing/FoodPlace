69f5ee83246535a55277746ed2b01c0f3d06df0b
ASHWHX <wanghaixin1993@126.com>
2020-04-16 23:48:58 +0100

    Add files via upload

---
+++ Controller/ControllerMyAccount.java
@@ -0,0 +1,61 @@
+package FoodPlace.Controller;
+
+import java.io.IOException;
+
+public class ControllerCustomerHome {
+    public Button linktomyaccount;
+    public Button linktomenu;
+    public Button linktomyorders;
+    public Button linktobookinghome;
+    public Button linktoedit;
+    public Button linktochangepassword;
+    public Button notificationspage;
+    public Button logoutbutton;
+
+    public void linktomyaccountButtonPressed(javafx.event.ActionEvent event) throws IOException {
+        Parent myaccountParent = FXMLLoader.load(getClass().getResource("MyAccount.fxml"));
+        Scene myaccountScene = new Scene(myaccountParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(myaccountScene);
+        window.show();
+    }
+
+    public void linktomenuButtonPressed(javafx.event.ActionEvent event) throws IOException {
+        Parent menuParent = FXMLLoader.load(getClass().getResource("customerMenu.fxml"));
+        Scene menuScene = new Scene(menuParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(menuScene);
+        window.show();
+    }
+
+    public void linktomyordersButtonPressed(javafx.event.ActionEvent event) throws IOException {
+        Parent myordersParent = FXMLLoader.load(getClass().getResource("myOrdersHome.fxml"));
+        Scene myordersScene = new Scene(myordersParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(myordersScene);
+        window.show();
+    }
+
+    public void linktobookinghomeButtonPressed(javafx.event.ActionEvent event) throws IOException {
+        Parent mybookingsParent = FXMLLoader.load(getClass().getResource("bookingHome.fxml"));
+        Scene mybookingsScene = new Scene(mybookingsParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(mybookingsScene);
+        window.show();
+    }
+
+    public void notificationspageButtonPressed(javafx.event.ActionEvent event) throws IOException {
+        Parent notificationsParent = FXMLLoader.load(getClass().getResource("Notification.fxml"));
+        Scene notificationsScene = new Scene(notificationsParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(notificationsScene);
+        window.show();
+    }
+
+    public void logoutbuttonPressed(javafx.event.ActionEvent event) throws IOException {
+        Parent logoutParent = FXMLLoader.load(getClass().getResource("Customer_login_Scene.fxml"));
+        Scene logoutScene = new Scene(logoutParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(logoutScene);
+        window.show();
+    }
