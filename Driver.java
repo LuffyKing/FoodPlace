3c1484982cee98aed8df8e8d63d07ffd9df988da
sara-r-p <62297420+sara-r-p@users.noreply.github.com>
2020-04-16 23:37:24 +0100

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,6 +1,22 @@
 package FoodPlace;
 
+/**
+*This class represents the delivery driver staff type and the functiond specific to them.
+*@author Damola Aderinwale (JavaDoc by Sara Philipson)
+*@version ?
+*/
 public class Driver extends Staff{
+    
+    /**
+    *Contructor for the delivery driver.
+    *@param fname First Name.
+    *@param lname Last Name.
+    *@param hoursToWork The number of hours the driver needs to work.
+    *@param sType The staff type (driver).
+    *@param sId The staff Id.
+    *@param uname The driver's username.
+    *@param pword The driver's password.
+    */
     public Driver(String fname,
                   String lname,
                   int hoursToWork,
@@ -17,7 +33,18 @@ public class Driver extends Staff{
                 uname,
                 pword);
     }
-
+    
+    /**
+    *Contructor for the delivery driver including the number of hours worked.
+    *@param fname First Name.
+    *@param lname Last Name.
+    *@param hoursToWork The number of hours the driver needs to work.
+    *@param sType The staff type (driver).
+    *@param sId The staff Id.
+    *@param uname The driver's username.
+    *@param pword The driver's password.
+    *@param hWorked The number of hours worked.
+    */
     public Driver(String fname,
                   String lname,
                   int hoursToWork,
@@ -36,9 +63,19 @@ public class Driver extends Staff{
                 pword,
                 hWorked);
     }
+    
+    /**
+    *Function to mark the delivery as being completed.
+    *@param delivery The delivery order to be updated.
+    */
     public void completeDelivery(Delivery delivery){
         delivery.setDeliveryStatus("Completed");
     }
+    
+    /**
+    *Function to mark the delivery as underway.
+    *@param delivery The delivery order to be updated.
+    */
     public void startDelivery(Delivery delivery){
         delivery.setDeliveryStatus("Started");
     }
