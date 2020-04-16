fefed7ca135197b85681c84aa648dee27dd65971
sara-r-p <62297420+sara-r-p@users.noreply.github.com>
2020-04-16 22:53:33 +0100

    Update BookingDB.java

--- FoodDB/BookingDB.java
+++ FoodDB/BookingDB.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 
 /**
 *Links the booking class to the sql database.
-*@author Damola Aderinwale
+*@author Damola Aderinwale (JavaDoc by Sara Philipson)
 *@version ?
 */
 public class BookingDB {
