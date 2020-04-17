3e7a98f97f4d094c86c46796a0205c22b3a13f57
ASHWHX <wanghaixin1993@126.com>
2020-04-17 05:03:21 +0100

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,4 +1,11 @@
 package FoodPlace;
+
+/**
+*This class represents a staff member who is a chef and the functions specific to this worker type.
+*@author Damola Aderinwale (JavaDoc by Haixin Wang)
+*@version ?
+*/
+
 import javafx.collections.ObservableList;
 
 import java.time.LocalDateTime;
@@ -21,43 +28,83 @@ abstract public class Order {
         this.customerId = customerId;
         this.isComplete = isComplete;
     }
-
+    
+    /**
+    *Returns the customer Id.
+    *@return The customer Id.
+    */
     public int getCustomerId() {
         return customerId;
     }
-
+    
+    /**
+    *Updates the customer Id.
+    *@param customerId The new customer Id.
+    */
     public void setCustomerId(int customerId) {
         this.customerId = customerId;
     }
 
+    /**
+    *Updates the order items.
+    *@param orderItems The new order items.
+    */
     public void setOrderItems(ObservableList<OrderItem> orderItems) {
         this.orderItems = orderItems;
     }
 
+    /**
+    *Returns the order item.
+    *@return The order item.
+    */
     public ObservableList<OrderItem> getOrderItems() {
         return orderItems;
     }
 
+    /**
+    *Returns the order id.
+    *@return The order id.
+    */
     public int getOrderId() {
         return orderId;
     }
 
+    /**
+    *Returns the order type.
+    *@return The order id.
+    */
     public String getOrderType() {
         return orderType;
     }
 
+    /**
+    *Updates the order type.
+    *@param orderType The new order type.
+    */
     public void setOrderType(String orderType) {
         this.orderType = orderType;
     }
 
+    /**
+    *Returns the date time.
+    *@return The date time.
+    */
     public LocalDateTime getDateTime() {
         return dateTime;
     }
 
+    /**
+    *Updates the data time.
+    *@param dateTime The new date time.
+    */
     public void setDateTime(LocalDateTime dateTime) {
         this.dateTime = dateTime;
     }
 
+    /**
+    *Function to count total order
+    *@param orderTotal The total orders.
+    */
     public double orderTotal(){
         double sum = 0 ;
         for(OrderItem item: orderItems){
@@ -66,14 +113,31 @@ abstract public class Order {
         return sum;
     }
 
+    /**
+    *Returns the status of whether complete or not.
+    *@return The status of completion.
+    */
     public boolean getCompletionStatus() {
         return isComplete;
     }
 
+    /**
+    *Updates the status of whether complete or not.
+    *@param isComplete The status of completion.
+    */
     public void setCompletionStatus(boolean status) {
         this.isComplete = status;
     }
 
+    /**
+    *Function to print the recept with order items and unit price and total.
+    *@param recept Get the new recept.
+    *@param count The number count.
+    *@param getName The order name.
+    *@param getUnitPrice the order unit price.
+    *@param getTotal the order total price.
+    *@return The actual recept with order items and unit price and total.
+    */
     public String[] printReceipt(){
         String[] receipt = new String[orderItems.size()+1];
         int count = 0;
